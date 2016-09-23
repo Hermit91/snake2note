45e5831bbcf7a1cb7a0f729d93897b8b2c39698e
Женечка <@BLUE_ROSE003>
2016-09-23 13:49:42 +0300

    перемещение символов

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,6 +10,19 @@ namespace snake
     {
         static void Main(string[] args)
         {
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(sym1);
+            Console.ReadLine();
+
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
